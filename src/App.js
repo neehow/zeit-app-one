983cbe256c5608a11f6db1ac481f3a54a65ad249
neehow <7135209+neehow@users.noreply.github.com>
2019-03-25 01:19:04 +0800

    change viewport meta according to material-uis doc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Time from './components/Time'
+import MaterialDemo from './components/MaterialDemo';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h3>你好，朋友！</h3>
         <Time />
+        <MaterialDemo />
       </div>
     );
   }
---
+++ src/components/MaterialDemo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from '@material-ui/core/Button'
+
+const MaterialDemo = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+)
+
+export default MaterialDemo
