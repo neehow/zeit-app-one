f6e537f88e3f41bdd10cedf199ac894dfe87f59c
neehow <7135209+neehow@users.noreply.github.com>
2019-03-24 16:11:25 +0800

    change: chore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h3>Hello, zeit now!</h3>
+        <h3>你好，朋友！</h3>
         <Time />
       </div>
     );
