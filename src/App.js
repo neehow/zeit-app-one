271ed3d372b7adadf54d9055e25998495803a087
neehow <7135209+neehow@users.noreply.github.com>
2019-03-26 00:18:40 +0800

    add Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import ColorConverter from './components/ColorConverter';
+import Footer from './components/Home/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ColorConverter /> 
+        <ColorConverter />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Home/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="text">
+        Hosted on&nbsp;
+        <a href="https://zeit.co/now" className="link" target="_blank" rel="noopener noreferrer">Now</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
